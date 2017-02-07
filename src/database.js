a31811a09ee7133f929e311b9fbfc312c374322e
mauro <maurop123@gmail.com>
2017-02-07 10:56:06 -0500

    firebase config in config.json

--- src/database.js
+++ src/database.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase'
+import config from './config.json'
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBaDMTMKZVFS1iS5oSWfMVvKB6akWODNUg",
-  authDomain: "mptysquare-295ed.firebaseapp.com",
-  databaseURL: "https://mptysquare-295ed.firebaseio.com",
-  storageBucket: "mptysquare-295ed.appspot.com",
-  messagingSenderId: "59554296307"
-})
+firebase.initializeApp(config.database)
 
 const db = firebase.database()
 
